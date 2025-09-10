16efc88fb96b134ea6dc6a4dcc2512bae7940c84
juanvasquezmarin <juanvasquez82231@elpoli.edu.co>
2025-09-10 13:45:05 -0500

    servidor montado

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+//Manejador de ruta
+
+async fn hello()-> impl Responder{
+    "hello, world!"
 }
+
+#[actix_web:: main]
+async fn main()-> std::io::Result<()>{
+    // Iniciar el servidor web
+    HttpServer::new(||{
+        App::new()
+        //Ruta y su correspondiente manejador
+        .route("/",web::get().to(hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+// 
